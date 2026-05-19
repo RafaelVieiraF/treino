21d032cafa51930b6472b08200c8591f691ac570
RafaelVieiraF <166732213+RafaelVieiraF@users.noreply.github.com>
2026-05-18 21:15:41 -0300

    primeiroenvio

---
+++ main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WorkoutChecklistApp from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <WorkoutChecklistApp />
+);
